1d1f40de5fc981c66ee116904299cd0a4f0a1e0b
Deepanshu Kumar <151247305+dk-0206@users.noreply.github.com>
2025-01-01 02:16:53 +0530

    Add files via upload

---
+++ src/Thriftly/ThriftlyTracker.java
@@ -0,0 +1,18 @@
+package Thriftly;
+
+/**
+ *
+ * @author Deepanshu Kumar
+ */
+public class ThriftlyTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Thriftly_Tracker().setLocationRelativeTo(null);
+        
+    }
+
+}
